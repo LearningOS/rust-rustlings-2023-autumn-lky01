137522e88196c0d04710614653ec6a55ba84bae7
lky01 <lky182671@gmail.com>
2023-10-10 18:38:03 +0600

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,8 +16,14 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(a:i32) -> i32 {
+    let a = if a<=40{
+        a*2
+    } else{
+        a
+    };
+    a
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
